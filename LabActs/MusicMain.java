98d4530753019decd597644c7084f3e7c2ae8a95
Adrian C. Alejandrino <adrian.alejandrino.1115@gmail.com>
2023-10-12 18:11:10 +0000

    hehe

--- LabActs/MusicMain.java
+++ LabActs/MusicMain.java
@@ -2,12 +2,9 @@ import java.util.*;
 
 public class MusicMain{
     static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args){
-        ArrayList<Song> songLib = new ArrayList<>();
-        int year;
-        String name;
-        String artist;
-        String category;
+        ArrayList<Object> songLib = new ArrayList<>();
 
         ArrayList<String> albums = new ArrayList<>();
         ArrayList<String> categs = new ArrayList<>();
@@ -15,6 +12,7 @@ public class MusicMain{
         int act = Menu();
         switch (act){
             case 1:
+                AddSong();
                 break;
             case 2:
                 break;
@@ -23,6 +21,10 @@ public class MusicMain{
             default:
                 break;
         }
+    }
+
+    static void EMssg(){
+        System.out.println("Error, please input again...");
     }
 
     static int Menu(){
@@ -39,8 +41,56 @@ public class MusicMain{
             if(act>0&&act<4){
                 break;
             }
-            System.out.println("Error, please input again...");
+            EMssg();
         }while(true);
         return act;
+    }
+
+    static void AddSong(){
+        int year;
+        String name;
+        String artist;
+        String[] categLib = {
+            "Hip-Hop", "Rock", "Pop", "Reggae", "Popular Music",
+            "Funk", "Folk Music", "Country", "Rhythm and Blues"
+        };
+        String categ;
+        String yrToString;
+
+        do{
+            System.out.println("Add a Song:\n");
+            System.out.print("Song name: ");
+            name = sc.nextLine();
+            System.out.print("Song artist: ");
+            artist = sc.nextLine();
+            while(true){
+                System.out.print("Year released: ");
+                if(sc.hasNextInt()){
+                    year = sc.nextInt();
+                    if(year<2024){
+                        yrToString = String.valueOf(year);
+                        break;
+                    }
+                }
+                EMssg();
+            }
+            System.out.println("Song Categories:");
+            for(int i=0;i<categLib.length;i++){
+                System.out.println("["+(i+1)+"] "+categLib[i]);
+            }
+            while(true){
+                System.out.print("Enter num of song category: ");
+                if(sc.hasNextInt()){
+                    int num = sc.nextInt();
+                    if(num>9){
+                        categ = categLib[num-1];
+                        break;
+                    }
+                }
+                EMssg();
+            }
+            
+            break;
+        }while(true);
     }
 }
