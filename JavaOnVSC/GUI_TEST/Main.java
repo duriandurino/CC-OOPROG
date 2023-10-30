1fc2d0add5594361c95caa415915aa14afc6abd1
duriandurino <adrian.alejandrino.1115@gmail.com>
2023-10-30 15:06:46 +0800

    guitest

--- JavaOnVSC/GUI_TEST/Main.java
+++ JavaOnVSC/GUI_TEST/Main.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
 
-class Main{
-    
+class Main extends JFrame{
 
+    void init(){
+
+        setTitle("DURINRIN");
+        setSize(800, 600);
+        setVisible(true);
+    }
 
     public static void main(String[]args){
-
+        Main m = new Main();
+        m.init();
     }
 }
